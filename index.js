fe61cc5f49839b5b807112ec141548c0c3474c0d
Mazharul Islam <mazharul.upsi.2017@gmail.com>
2021-11-01 20:41:22 +0800

    added

--- index.js
+++ index.js
@@ -176,6 +176,14 @@ async function run(){
         res.json(service);
     })
 
+    // Delete Operation
+    app.delete('/order/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = { _id: ObjectId(id)};
+        const result = await allOrders.deleteOne(query);
+        res.json(result);
+    });
+
    
 
     //  POST API
